9ec098a35c32c459cf27d88d2e5d87e9443ac947
manish-giri <manish.giri.me@gmail.com>
2020-07-10 05:42:27 -0400

    Fix cypress test typo

--- cypress/integration/learn/index.js
+++ cypress/integration/learn/index.js
@@ -35,7 +35,7 @@ describe('Learn Landing page', function() {
     cy.contains('h1', 'Welcome to freeCodeCamp.org');
   });
 
-  it('renders a curriuculum map', () => {
+  it('renders a curriculum map', () => {
     cy.document().then(document => {
       const superBlocks = document.querySelectorAll(
         `${selectors.challengeMap} > ul > li`
